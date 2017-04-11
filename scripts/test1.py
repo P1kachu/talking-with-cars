d9b11647a835ab543f91febd06aa9e387c557374
Stanislas P1kachu Lejay <p1kachu@lse.epita.fr>
2017-04-11 23:56:46 +0200

    update test1
    Signed-off-by: Stanislas P1kachu Lejay <p1kachu@lse.epita.fr>

--- scripts/test1.py
+++ scripts/test1.py
@@ -5,8 +5,8 @@ import can
 INTERFACE="vcan0"
 
 bus = can.interface.Bus(channel=INTERFACE, bustype='socketcan_native')
-msg = can.Message(arbitration_id=0x7de,
-        data = [0, 25, 0, 1, 3, 1, 4, 1],
-        extended_id=False)
+notifier = can.Notifier(bus, [can.Printer('can.log')])
+msg = can.Message(arbitration_id=0x7de, data = [0, 25, 0, 1, 3, 1, 4, 1], extended_id=False)
 bus.send(msg)
-#notifier = can.Notifier(bus, [can.Printer()])
+msg = can.Message(arbitration_id=0x7df, data = [2, 1, 5, 0, 0, 0, 0, 0], extended_id=False)
+bus.send(msg)
