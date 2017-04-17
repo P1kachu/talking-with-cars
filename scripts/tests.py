2c9cca61d07c4975b6e6da881b9ea9e60310ccd1
Stanislas P1kachu Lejay <p1kachu@lse.epita.fr>
2017-04-17 13:38:48 +0200

    Add first VW Polo R6 fuzzing results
    Signed-off-by: Stanislas P1kachu Lejay <p1kachu@lse.epita.fr>

--- scripts/tests.py
+++ scripts/tests.py
@@ -16,17 +16,9 @@ def can_xchg(bus, arb_id, data, extended=False):
     msg = can.Message(arbitration_id=arb_id, data=data, extended_id=extended)
     bus.send(msg)
     #print("TX: {0}: {1})".format(hex(msg.arbitration_id), msg.data))
-    answer = bus.recv()
+    answer = bus.recv(0.2)
     #print("RX: {0}: {1})".format(hex(answer.arbitration_id), answer.data))
     return answer
-
-def bf(bus):
-    for y in range(0xff):
-        for x in range(0xff):
-            answer = can_xchg(bus, 0x7df, [3, 1, x, y, 0, 0, 0, 0])
-            if answer is not None:
-                print(answer)
-
 
 bus = can.interface.Bus(channel=INTERFACE, bustype='socketcan_native')
 
@@ -53,6 +45,7 @@ answer = can_xchg(bus, 0x7df, [2, 1, 0x11, 0, 0, 0, 0, 0], False)
 print("Throttle position: {0}%".format(answer.data[3]))
 '''
 
+'''
 # Mode 1 supported PIDs
 answer = can_xchg(bus, 0x7df, [2, 1, 0x00, 0, 0, 0, 0, 0])
 print(get_bits_msb(4, answer.data))
@@ -60,12 +53,13 @@ answer = can_xchg(bus, 0x7df, [2, 1, 0x20, 0, 0, 0, 0, 0])
 print(get_bits_msb(4, answer.data))
 answer = can_xchg(bus, 0x7df, [2, 1, 0x40, 0, 0, 0, 0, 0])
 print(get_bits_msb(4, answer.data))
-answer = can_xchg(bus, 0x7df, [2, 1, 0x60, 0, 0, 0, 0, 0])
-print(get_bits_msb(4, answer.data))
+#answer = can_xchg(bus, 0x7df, [2, 1, 0x60, 0, 0, 0, 0, 0])
+#print(get_bits_msb(4, answer.data))
 
 # Mode 9 supported PIDs
 answer = can_xchg(bus, 0x7df, [2, 9, 0x00, 0, 0, 0, 0, 0])
 bits = get_bits_msb(4, answer.data)
+print(bits)
 if bits[0xa]:
     answer = can_xchg(bus, 0x7df, [2, 9, 0x09, 0, 0, 0, 0, 0])
     answer = can_xchg(bus, 0x7df, [2, 9, 0x0a, 0, 0, 0, 0, 0])
@@ -73,4 +67,25 @@ if bits[0xa]:
         print(bus.recv())
 else:
     print("PID 9:0xa not supported")
+'''
 
+def bruteforce_byte_0(bus, arb_id):
+    for i in range(0x20):
+        can_xchg(bus, arb_id, [i, 0, 0, 0, 0, 0, 0, 0])
+        can_xchg(bus, arb_id, [i, 64, 64, 64, 64, 64, 64, 64]) 
+        can_xchg(bus, arb_id, [i, 128, 128, 128, 128, 128, 128, 128])
+        can_xchg(bus, arb_id, [i, 255, 255, 255, 255, 255, 255, 255]) 
+
+def bruteforce_byte_1(bus, arb_id):
+    for i in range(0x20):
+        can_xchg(bus, arb_id, [1, i, 0, 0, 0, 0, 0, 0])
+    for i in range(0x20):
+        can_xchg(bus, arb_id, [1, i, 64, 64, 64, 64, 64, 64]) 
+    for i in range(0x20):
+        can_xchg(bus, arb_id, [1, i, 128, 128, 128, 128, 128, 128])
+    for i in range(0x20):
+        can_xchg(bus, arb_id, [1, i, 255, 255, 255, 255, 255, 255]) 
+
+#bruteforce_byte_0(bus, 0x711)
+#bruteforce_byte_0(bus, 0x711)
+bruteforce_byte_1(bus, 0x7f1)
