b4c31642575eac8f24f5f07263a79016a094fb77
Stanislas P1kachu Lejay <p1kachu@lse.epita.fr>
2017-05-06 20:18:13 +0200

    Update kwp2000 tests
    Signed-off-by: Stanislas P1kachu Lejay <p1kachu@lse.epita.fr>

--- scripts/kwp2000.py
+++ scripts/kwp2000.py
@@ -5,10 +5,17 @@ from time import sleep
 
 INTERFACE="can0"
 
-# Diagnostic session init
 EXTENDED_DIAGNOSTIC = 0x3
+DIAGNOSTIC_SESSION = 0x10
+SECURITY_SESSION = 0x27
 
-# Security session init
+GENERAL_REJECT = 0x10
+BUSY_REPEAT_REQUEST = 0x21
+SECU_SESSION_BAD_KEY = 0x35
+SECU_SESSION_EXCEED_NUMBER = 0x36
+SECU_SESSION_DELAY_NOT_EXPIRED = 0x37
+
+IGNORE_KEY = -1
 REQUEST_SEED = 0x1
 SEND_KEY = 0x2
 VW_REQUEST_SEED = 0x3 # Different levels of security defined by the manufacturer.
@@ -18,17 +25,20 @@ def can_xchg(bus, arb_id, data, extended=False):
     msg = can.Message(arbitration_id=arb_id, data=data, extended_id=extended)
     bus.send(msg)
     print("TX: {0}".format(msg))
-    answer = bus.recv(0.25)
+
+    answer = bus.recv(0.1)
     if answer is None:
         return
+
     print("RX: {0}".format(answer))
     return answer
 
 def kwp2000_init_diagnostic_session(bus, arb_id, session_type=EXTENDED_DIAGNOSTIC):
-    answer = can_xchg(bus, arb_id, [2, 0x10, session_type, 0, 0, 0, 0, 0])
+    answer = can_xchg(bus, arb_id, [2, DIAGNOSTIC_SESSION, session_type, 0, 0, 0, 0, 0])
 
-    if answer and answer.data[1] == (0x50):
-        print("Diagnostic session susccessfuly opened")
+    if answer and answer.data[1] == (DIAGNOSTIC_SESSION + 0x40):
+        #print("Diagnostic session susccessfuly opened")
+        pass
     else:
         print("Failure in establishing ISO-TP diagnostic session")
         return None
@@ -36,80 +46,83 @@ def kwp2000_init_diagnostic_session(bus, arb_id, session_type=EXTENDED_DIAGNOSTI
     return answer
 
 def compute_key(seed):
-    # http://nefariousmotorsports.com/forum/index.php?topic=4983.msg50819#msg50819
-    seed[1] += 0x01
-    seed[2] += 0x11
-    seed[3] += 0x70
     return seed
 
-def kwp2000_init_security_session(bus, arb_id, req_seed=REQUEST_SEED, send_key=SEND_KEY):
-    answer = can_xchg(bus, arb_id, [2, 0x27, req_seed, 0, 0, 0, 0, 0])
+def _kwp2000_init_security_session_error(code):
+    if code == SECU_SESSION_BAD_KEY:
+        print("Bad key in security session init")
+    elif code == SECU_SESSION_EXCEED_NUMBER:
+        print("ExceedNumberOfAttempts in security session init")
+    elif code == SECU_SESSION_DELAY_NOT_EXPIRED:
+        print("requiredTimeDelayNotExpired in security session init")
+    elif code == BUSY_REPEAT_REQUEST:
+        print("BUSY_REPEAT_REQUEST - sleeping")
+        sleep(0.3)
+    else:
+        print("Failure in sending security session key")
 
-    if answer and answer.data[1] == 0x67:
+
+def kwp2000_init_security_session(bus, arb_id, req_seed=REQUEST_SEED, send_key=SEND_KEY):
+    answer = can_xchg(bus, arb_id, [2, SECURITY_SESSION, req_seed, 0, 0, 0, 0, 0])
+
+    if answer and answer.data[1] == (SECURITY_SESSION + 0x40):
         seed = list(answer.data[3:7])
         print("Security session seed: {0}".format(seed))
     else:
-        print("Failure in establishing ISO-TP security session")
+        _kwp2000_init_security_session_error(answer.data[3])
         return None
 
-    data_key = [6, 0x27, send_key] + compute_key(seed) + [0]
-    answer = can_xchg(bus, arb_id, [6, 0x27, send_key, 0, 0, 0, 0, 0])
+    if send_key == IGNORE_KEY:
+        return None
 
-    if answer and answer.data[1] == 0x67:
-        print("Security session opened!")
-    elif answer and answer.data[3] == 0x35:
-        print("Bad key in security session init")
-        return None
-    elif answer and answer.data[3] == 0x36:
-        print("ExceedNumberOfAttempts in security session init")
-        return None
-    elif answer and answer.data[3] == 0x37:
-        print("requiredTimeDelayNotExpired in security session init")
-        return None
+    data_key = [6, SECURITY_SESSION, send_key] + compute_key(seed) + [0]
+    answer = can_xchg(bus, arb_id, data_key)
+
+    if answer and answer.data[1] == (SECURITY_SESSION + 0x40):
+        #print("Security session opened!")
+        pass
     else:
-        print("Failure in sending security session key")
+        _kwp2000_init_security_session_error(answer.data[3])
         return None
 
     return answer
 
-def kwp2000_init_upload_session(bus, arb_id):
-    # Not tested right now
-    # From http://nefariousmotorsports.com/forum/index.php?topic=4983.msg51246#msg51246
-    #answer = can_xchg(bus, arb_id, [0x9, 0x35, 0x00, 0x00, 0x00, 0x00, 0x20, 0x00, 0x00])
-    return answer
+def kwp2000_test_security_session(bus, key=SEND_KEY, arb_id=0x7e0):
+    iteration_nb = 15
 
-def kwp2000_test_security_session(bus):
-    if kwp2000_init_diagnostic_session(bus, 0x7e0):
-        while True:
-            if kwp2000_init_security_session(bus, 0x7e0, VW_REQUEST_SEED, VW_SEND_KEY):
+    while True:
+        if kwp2000_init_diagnostic_session(bus, arb_id):
+            if kwp2000_init_security_session(bus, arb_id, VW_REQUEST_SEED, key):
                 print("GO DUMP STUFF")
-                kwp2000_init_upload_session(bus, 0x7e0)
-            else:
+                return
+            elif key != IGNORE_KEY:
                 print("Failed key exchange - Sleeping")
                 sleep(5)
 
-def kwp2000_test_other_ecu(bus):
-    possible_ids = [0x077e, 0x077b, 0x077d, 0x07e8, 0x07d6, 0x0774, 0x07bb, 0x077f]
+        iteration_nb -= 1
+        if iteration_nb == 0:
+            return
+
+def kwp2000_test_other_ecus(bus):
+    possible_ids = [0x0700, 0x0703, 0x0704, 0x0706, 0x0707, 0x0708, 0x070A,
+                    0x0711, 0x0713, 0x0714, 0x0715, 0x0751, 0x076C, 0x07E0,
+                    0x07F1]
     for arb_id in possible_ids:
         if kwp2000_init_diagnostic_session(bus, arb_id):
-            print("ID {0} responded".format(arb_id))
-            kwp2000_init_security_session(bus, arb_id, VW_REQUEST_SEED, VW_SEND_KEY)
+            kwp2000_test_security_session(bus, key=IGNORE_KEY, arb_id=arb_id)
 
-def kwp2000_test_other_ecu(bus):
-    possible_ids = [0x077e, 0x077b, 0x077d, 0x07e8, 0x07d6, 0x0774, 0x07bb, 0x077f]
-    for arb_id in possible_ids:
-        if kwp2000_init_diagnostic_session(bus, arb_id):
-            print("ID {0} responded".format(arb_id))
-            kwp2000_init_security_session(bus, arb_id, VW_REQUEST_SEED, VW_SEND_KEY)
-
-def kwp2000_test_readall(bus):
-    if kwp2000_init_diagnostic_session(bus, 0x7e0):
+def kwp2000_test_readall(bus, arb_id=0x7e0):
+    if kwp2000_init_diagnostic_session(bus, arb_id):
         for high in range(0xff):
             for low in range(0xff):
-                answer = can_xchg(bus, 0x7e0, [5, 0x22, high, low, 0x1, 0, 0, 0, 0])
+                answer = can_xchg(bus, arb_id, [5, 0x22, high, low, 0x1, 0x1, 0, 0])
+                sleep(0.05)
+
+
+
+#### MAIN ####
 
 bus = can.interface.Bus(channel=INTERFACE, bustype='socketcan_native')
-kwp2000_test_security_session(bus)
-#kwp2000_test_other_ecu(bus)
-#kwp2000_test_readall(bus)
+#kwp2000_test_other_ecus(bus)
+kwp2000_test_readall(bus)
 
