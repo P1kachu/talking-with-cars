eb27762720adb4ec4d1e4ce0398ca476d25e2cd3
Stanislas P1kachu Lejay <p1kachu@lse.epita.fr>
2017-06-13 13:44:53 +0200

    Forget Grid, let's go for Vdrift
    Signed-off-by: Stanislas P1kachu Lejay <p1kachu@lse.epita.fr>

--- scripts/cangamepad_poc_game.py
+++ scripts/cangamepad_poc_game.py
@@ -1,163 +1,71 @@
-#!/usr/bin/env python3
-import foohid
-import struct
-import random
+import uinput
 import time
-import socket
+import random
 
+CONTROLLER = "Microsoft X-Box 360 pad - P1ka"
+VENDOR = 0x45e
+PRODUCT = 0x28e
+VERSION = 101
 
-PORT=12345
-DEVICE_NAME="FooHID CAN Joypad"
-STEERING_MAX_ABS_CNST = 0x450 * 2
+def main():
+    events = (
+        uinput.BTN_SOUTH,                       # Handbrake
+        uinput.BTN_EAST,
+        uinput.BTN_NORTH,
+        uinput.BTN_WEST,                        # TODO: Change camera
+        uinput.BTN_TL,                          # TODO: Gear down
+        uinput.BTN_TR,                          # TODO: Gear up
+        uinput.BTN_SELECT,
+        uinput.BTN_START,
+        uinput.BTN_MODE,
+        uinput.BTN_THUMBL,
+        uinput.BTN_THUMBR,
 
-keyboard = (
-    0x05, 0x01,
-    0x09, 0x06,
-    0xa1, 0x01,
-    0x05, 0x07,
-    0x19, 0xe0,
-    0x29, 0xe7,
-    0x15, 0x00,
-    0x25, 0x01,
-    0x75, 0x01,
-    0x95, 0x08,
-    0x81, 0x02,
-    0x95, 0x01,
-    0x75, 0x08,
-    0x81, 0x01,
-    0x95, 0x05,
-    0x75, 0x01,
-    0x05, 0x08,
-    0x19, 0x01,
-    0x29, 0x05,
-    0x91, 0x02,
-    0x95, 0x01,
-    0x75, 0x03,
-    0x91, 0x01,
-    0x95, 0x06,
-    0x75, 0x08,
-    0x15, 0x00,
-    0x25, 0x65,
-    0x05, 0x07,
-    0x19, 0x00,
-    0x29, 0x65,
-    0x81, 0x00,
-    0x09, 0x00,
-    0x75, 0x08,
-    0x95, 0x01,
-    0x15, 0x00,
-    0x25, 0x7f,
-    0xb1, 0x02,
-    0xc0
-)
+        uinput.ABS_X + (-32768, 32767, 0, 0),   # Steering wheel
+        uinput.ABS_Y + (-32768, 32767, 0, 0),
+        uinput.ABS_RX + (-32768, 32767, 0, 0),
+        uinput.ABS_RY + (-32768, 32767, 0, 0),
+        uinput.ABS_GAS + (0, 255, 0, 0),        # Accelerator
+        uinput.ABS_BRAKE + (0, 255, 0, 0),      # Brakes
+        uinput.ABS_HAT0X + (-1, 1, 0, 0),
+        uinput.ABS_HAT0Y + (-1, 1, 0, 0),
 
-def convert(data):
-    return struct.unpack_from("BBBBBI", data)
+        )
 
-def get_steering(steering_angle):
-    steering_angle = int((steering_angle / STEERING_MAX_ABS_CNST) * 255)
-    return steering_angle
+    array = [
+        uinput.BTN_SOUTH,                       # Handbrake
+        uinput.BTN_EAST,
+        uinput.BTN_NORTH,
+        uinput.BTN_WEST,                        # TODO: Change camera
+        uinput.BTN_TL,                          # TODO: Gear down
+        uinput.BTN_TR,                          # TODO: Gear up
+        uinput.BTN_SELECT,
+        uinput.BTN_START,
+        uinput.BTN_MODE,
+        uinput.BTN_THUMBL,
+        uinput.BTN_THUMBR,
+        ]
 
-def create_output_report(data):
-    speed, handbrake, clutch, brakes, accelerator, steering_angle = convert(data)
-    print(speed, handbrake, clutch, brakes, accelerator, steering_angle)
-    steering = get_steering(steering_angle)
-    return (speed, handbrake, clutch, brakes, accelerator, steering)
+    with uinput.Device(events,
+                       name=CONTROLLER,
+                       vendor=VENDOR,
+                       product=PRODUCT,
+                       bustype=3,
+                       version=VERSION,
+                       ) as device:
+        print("Device created")
+        time.sleep(1)
+        device.emit_click(uinput.BTN_MODE)
+        for i in range(200000):
+            print(i)
+            r1 = random.randint(-32767, 32767)
+            r2 = random.randint(-32767, 32767)
+            r3 = random.randint(-32767, 32767)
+            device.emit(uinput.ABS_X, r1, syn=False)
+            device.emit(uinput.ABS_GAS, r2, syn=False)
+            device.emit(uinput.ABS_BRAKE, r3, syn=False)
+            device.emit_click(array[i % (len(array) - 1)], syn=True)
+            time.sleep(0.3)
 
-try:
-    foohid.send("FooHID simple keyboard", struct.pack('8B', 0, 0, 0, 0, 0, 0, 0, 0))
-    foohid.destroy("FooHID simple keyboard")
-except:
-    pass
-
-try:
-    foohid.create("FooHID simple keyboard",
-            struct.pack('{0}B'.format(len(keyboard)), *keyboard),
-            "SN 123",
-            2,
-            3)
-except:
-    foohid.send("FooHID simple keyboard", struct.pack('8B', 0, 0, 80, 0, 0, 0, 0, 0))
-    exit(0)
-
-print("Created device")
-
-s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-s.bind(("", PORT))
-print("Created socket")
-
-accel = 20
-steering = 127
-prev_steering = 127
-
-keys = [0,0,0,0]
-tmp_keys = [0,0,0,0]
-try:
-    while True:
-        if 0: # DEBUG
-            accel -= 1
-            if accel == -20:
-                accel = 20
-            accelerator = accel
-            brakes = (accel < - 10)
-            handbrake = (accel < - 15)
-            steering += 1
-            if steering >= 255:
-                steering = 0
-        else:
-            data = s.recv(1024)
-            speed, handbrake, clutch, brakes, accelerator, steering = create_output_report(data)
-
-        keys[0] = 0
-        keys[1] = 0
-        keys[2] = 0
-        keys[3] = 0
-
-        if handbrake:
-            keys[0] = 0x2c
-        if accelerator > 0:
-            keys[1] = 0x52
-        if steering < 120 or prev_steering < steering - 100:
-            keys[2] = 0x50
-        elif steering > 134 or prev_steering > steering + 100:
-            keys[2] = 0x4f
-        else:
-            keys[2] = 0
-        if brakes:
-            keys[3] = 0x51
-
-        prev_steering = steering
-
-        reset = False
-        for i in range(4):
-            if keys[i] != tmp_keys[i]:
-                reset = True
-                #print("Reset")
-            tmp_keys[i] = keys[i]
-
-        if reset:
-            #foohid.send("FooHID simple keyboard", struct.pack('8B', 0, 0, 0, 0, 0, 0, 0, 0))
-            #time.sleep(1)
-            pass
-        else:
-            #time.sleep(0.2)
-            pass
-
-        foohid.send("FooHID simple keyboard",
-                    struct.pack('8B',
-                        0,
-                        0,
-                        keys[0],
-                        keys[1],
-                        keys[2],
-                        keys[3],
-                        0,
-                        0)
-                    )
-
-except Exception as e:
-    print(e)
-    # make sure key is unpressed before exiting
-    foohid.send("FooHID simple keyboard", struct.pack('8B', 0, 0, 0, 0, 0, 0, 0, 0))
-    foohid.destroy("FooHID simple keyboard")
-
+if __name__ == "__main__":
+    main()
