c64ebe24d963f69fa71f5c182d8a3f2146f51a0d
Stanislas P1kachu Lejay <p1kachu@lse.epita.fr>
2017-07-10 09:59:13 +0200

    Update CANPad2 for reduced steering
    Signed-off-by: Stanislas P1kachu Lejay <p1kachu@lse.epita.fr>

--- scripts/CANPad_game_v2.py
+++ scripts/CANPad_game_v2.py
@@ -11,10 +11,28 @@ from evdev import UInput, InputDevice, ecodes as e, InputEvent
 # to send data over evdev by hijacking your controller
 # Settings for Dirt Showdown
 
+# Path to XBox evdev
 DEFAULT_XBOX_CONTROLLER_PATH = '/dev/input/event13'
+
+# UDP port to receive data from
 PORT=12345
+
+# Max steering angle sent by car
 STEERING_MAX_ABS_CNST = 0x450
+
+# Max steering angle understood by game
+# This allows one to fully steer in the game
+# without the need to fully steer in the car
+# when FULL_WHEEL_ENABLED is False
+STEERING_MAX_VIRTUAL_CNST = int(STEERING_MAX_ABS_CNST / 5)
+FULL_WHEEL_ENABLED = False
+
+# Max value taken by the gamepad
 STEERING_MAX_GAME_CNST = 32767
+
+VERBOSE = False
+
+# Gamepad buttons mapping
 HANDBRAKE_BUTTON = e.BTN_EAST
 STEERING_BUTTON = e.ABS_X
 ACCELERATOR_BUTTON = e.ABS_RZ
@@ -26,7 +44,10 @@ def convert(data):
 
 def get_steering(steering_angle):
     steering_angle -= STEERING_MAX_ABS_CNST
-    steering_angle = int((steering_angle / STEERING_MAX_ABS_CNST) * STEERING_MAX_GAME_CNST)
+    if FULL_WHEEL_ENABLED:
+        steering_angle = int((steering_angle / STEERING_MAX_ABS_CNST) * STEERING_MAX_GAME_CNST)
+    else:
+        steering_angle = int((steering_angle / STEERING_MAX_VIRTUAL_CNST) * STEERING_MAX_GAME_CNST)
     return steering_angle
 
 def parse_data(data):
@@ -34,7 +55,6 @@ def parse_data(data):
     steering = get_steering(steering_angle)
     brakes = int(brakes/2 * 255)
     accelerator = int(min(accelerator / 1, 1) * 255)
-    #print(speed, handbrake, clutch, brakes, accelerator, steering_angle)
     return (speed, handbrake, clutch, brakes, accelerator, steering)
 
 
@@ -42,12 +62,14 @@ try:
     if len(sys.argv) > 1:
         gamepad_path = sys.argv[1]
     else:
+        print("[*] Usage: {0} PATH_TO_XBOX_EVDEV".format(sys.argv[0]))
+        print("[*] Defaulting to path: {0}".format(DEFAULT_XBOX_CONTROLLER_PATH))
         gamepad_path = DEFAULT_XBOX_CONTROLLER_PATH
 
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     s.bind(("", PORT))
     s.setblocking(0)
-    print("Created socket")
+    print("[*] Created UDP socket")
 
 
     xbox = InputDevice(gamepad_path)
@@ -58,7 +80,7 @@ try:
         except:
             pass
 
-    print("Waiting for input")
+    print("[*] Waiting for input")
     while True:
         try:
             newdata = s.recv(1024)
@@ -80,8 +102,8 @@ try:
         xbox.write(e.EV_ABS, BRAKES_BUTTON, brakes)
         xbox.write_event(InputEvent(1334414993, 274296, e.EV_SYN, 0, 0))
 
-        #print(handbrake, brakes, accelerator, steering_angle)
-        #time.sleep(0.3)
+        if VERBOSE:
+            print(handbrake, brakes, accelerator, steering_angle)
 except Exception as e:
     print(e)
     pass
