ddd7a2b7137a67aeac84a0eb858b57bc08aee38c
Stanislas P1kachu Lejay <p1kachu@lse.epita.fr>
2021-10-11 16:37:06 +0200

    Add some random can logs
    Signed-off-by: Stanislas P1kachu Lejay <p1kachu@lse.epita.fr>

--- scripts/simulate.sh
+++ scripts/simulate.sh
@@ -3,7 +3,14 @@
 INTERFACE=vcan0
 ID=7E8
 
-while true; do
+if [ "$INTERFACE" = "vcan0" ]; then
+        modprobe vcan
+        sudo ip link add dev vcan0 type vcan
+        sudo ip link set up vcan0
+fi
+
+
+while false; do
 	cansend $INTERFACE $ID#02410540     	   # Coolant
 	cansend $INTERFACE $ID#04410C4040   	   # RPM
 	cansend $INTERFACE $ID#04410D40     	   # Speed
@@ -16,4 +23,16 @@ while true; do
 	cansend $INTERFACE 0810a000#AAAA20         # Brakes
 	cansend $INTERFACE $ID#04414940     	   # Accelerator
 	sleep 0.2
+done
+
+while true; do
+        while read line; do
+                echo $line
+                id=$(echo $line |cut -d" " -f5)
+                data=$(echo $line |cut -d" " -f7-15|sed 's/ //g'|cut -d"'" -f1)
+                #echo $INTERFACE $id\#$data
+                cansend $INTERFACE $id\#$data
+                sleep 0.01
+        done <test_nissan_leaf_traffic.log
+        echo "Loop back"
 done
